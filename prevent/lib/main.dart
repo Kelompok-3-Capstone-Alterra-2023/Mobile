8d41e82dd78b43db826436496dc3c96fb711083f
FTio <Tiofederick@gmail.com>
2023-05-22 20:07:19 +0700

    slicing forgot and reset password

--- prevent/lib/main.dart
+++ prevent/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:prevent/Screen/home_page.dart';
+import 'package:prevent/Screen/forgot_page.dart';
 
 void main() {
   runApp(const MyApp());
