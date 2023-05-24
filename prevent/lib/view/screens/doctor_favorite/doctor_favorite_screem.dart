2e91d8ad6d725fbe43d5fe84b2ca3c3ce411f44f
YahyaAiman <yahyaaiman95@gmail.com>
2023-05-24 15:02:19 +0700

    Adding Doctor Favorite Screen

---
+++ prevent/lib/view/screens/doctor_favorite/doctor_favorite_screem.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:prevent/util/theme.dart';
+import 'package:prevent/view/widgets/foz_button.dart';
+
+class DoctorFavoriteScreen extends StatelessWidget {
+  const DoctorFavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        leadingWidth: 30,
+        backgroundColor: whiteColor,
+        leading: IconButton(
+            splashRadius: 18,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: blackColor,
+            )),
+        elevation: 0,
+        title: Text(
+          'Doctor Favorite',
+          style: GoogleFonts.inter(
+              color: blackColor, fontWeight: semiBold, fontSize: 16),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+        child: GridView.builder(
+          itemCount: 6,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            // mainAxisExtent: 285,
+            childAspectRatio: 0.62,
+            crossAxisSpacing: 9,
+            mainAxisSpacing: 50,
+          ),
+          itemBuilder: (context, index) {
+            return GridTile(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 14, horizontal: 13),
+                decoration: BoxDecoration(
+                  color: colorStyleThird,
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Align(
+                      alignment: Alignment.center,
+                      child: CircleAvatar(
+                        radius: 62,
+                        backgroundImage:
+                            AssetImage('assets/images/doctor_image.png'),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Farid Ahmad S.Psi, M.Psi',
+                      style: GoogleFonts.inter(
+                        fontWeight: semiBold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Text(
+                      'Psikologi Klinis Dewasa',
+                      style: GoogleFonts.inter(
+                        fontWeight: reguler,
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      'Gangguan kecemasan',
+                      style: GoogleFonts.inter(
+                        fontWeight: reguler,
+                        fontSize: 12,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Row(mainAxisSize: MainAxisSize.min, children: [
+                      const Icon(
+                        Icons.work,
+                        size: 11,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        '12 Tahun',
+                        style: GoogleFonts.poppins(
+                            fontSize: 11, fontWeight: medium),
+                      )
+                    ]),
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: FozButton(
+                          onPressed: () {
+                            // TODO: Navigasi konsultasi
+                          },
+                          height: 35,
+                          width: 81,
+                          buttonColor: colorStyleFifth,
+                          child: Text(
+                            'Konsultasi',
+                            style: GoogleFonts.inter(
+                                fontWeight: semiBold,
+                                fontSize: 12,
+                                color: whiteColor),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
