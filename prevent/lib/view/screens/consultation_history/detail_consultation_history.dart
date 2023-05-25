1a20ff362cb61a324fd2cf5a78308e4cc1583ac7
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2023-05-25 09:54:51 +0700

    remove unused packages

--- prevent/lib/view/screens/consultation_history/detail_consultation_history.dart
+++ prevent/lib/view/screens/consultation_history/detail_consultation_history.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:iconify_flutter/iconify_flutter.dart';
-import 'package:iconify_flutter/icons/ri.dart';
 import 'package:intl/intl.dart';
 
 import '../../../util/theme.dart';
